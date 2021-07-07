fba5ee6c0bee722519f9a8bb3901d7b37f17b7c0
Landscape <494326656@qq.com>
2021-07-07 18:21:56 +0800

    1

---
+++ support/src/main/java/com/landside/support/layoutcontainer/PopupLayoutContainer.kt
@@ -0,0 +1,6 @@
+package com.landside.support.layoutcontainer
+
+import android.view.View
+import kotlinx.android.extensions.LayoutContainer
+
+class PopupLayoutContainer(override val containerView: View) : LayoutContainer
